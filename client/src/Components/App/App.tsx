d647091fd48a89ee463d9721f930e5450439f875
NikitosPes <kutsokon95@gmail.com>
2022-04-14 21:56:01 +0300

    Start working on MusicList.

--- client/src/Components/App/App.tsx
+++ client/src/Components/App/App.tsx
@@ -1,8 +1,11 @@
 import Header from "../Header/Header";
+import MusicList from "../MusicList/MusicList";
+import MusicListItem from "../MusicListItem/MusicListItem";
 
 function App() {
     return <div>
         <Header/>
+        <MusicList title="Recomendation"/>
     </div>
 }
 
